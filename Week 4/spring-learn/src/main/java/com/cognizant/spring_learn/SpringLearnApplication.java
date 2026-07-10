5f79e173134275019a5359107ea0b8e3a4d6e56f
SwayamKhandelwal <swayam.khandelwal.9082@gmail.com>
2026-07-10 23:27:02 +0530

    Ex 4 Done

--- Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,14 +1,30 @@
 package com.cognizant.spring_learn;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLearnApplication.class, args);
-		System.out.println("Application Running Successfully");
+
+		displayCountry();
+
 	}
 
+	public static void displayCountry() {
+
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("country.xml");
+
+		Country country =
+				context.getBean("country", Country.class);
+
+		System.out.println(country);
+
+	}
 }
