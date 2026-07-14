5a486083d5edb744308942474a78d6a56c83038c
SwayamKhandelwal <swayam.khandelwal.9082@gmail.com>
2026-07-14 23:25:32 +0530

    Get country by code done

--- Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
+++ Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -1,6 +1,9 @@
 package com.cognizant.spring_learn.controller;
 
+import com.cognizant.spring_learn.Country;
 import com.cognizant.spring_learn.SpringLearnApplication;
+import com.cognizant.spring_learn.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +17,12 @@ public class CountryController {
     public String getCountryIndia(){
         return SpringLearnApplication.displayCountry();
     }
+    @Autowired
+    private CountryService countryService;
 
     @GetMapping("/countries/{code}")
-    public String getCountry(@PathVariable String code){
-        return
+    public Country getCountry(@PathVariable String code) {
+
+        return countryService.getCountry(code);
     }
 }
