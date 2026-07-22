464086534362d1896ac67dbe57bd107e40399bcd
SwayamKhandelwal <swayam.khandelwal.9082@gmail.com>
2026-07-23 00:14:07 +0530

    Sl4j exercise done

---
+++ Week 5/J_Unit/src/main/java/in/springdemo/LoggingExample.java
@@ -0,0 +1,18 @@
+package in.springdemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingExample {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingExample.class);
+
+    public static void main(String[] args) {
+
+        logger.error("this is an error message");
+
+
+        logger.warn("this is a warning message");
+    }
+}
